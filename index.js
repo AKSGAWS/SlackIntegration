0b02565217b9e7b0ab619469a9973c7c71e93bde
AKSGAWS <111831931+AKSGAWS@users.noreply.github.com>
2023-04-11 10:06:41 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -34,34 +34,27 @@ bot.on('message', (data) => {
 
 // Resppond to Data
 function handleMessage(message){
-/*  let msg = message.split(' ');
-  console.log("First value is " + msg[0]);
-  console.log("Second (Country) value is " + msg[1]);
-  console.log("Third (Parameter) value is " + msg[2]);
-  if(msg[2].includes('timezone')) {
-    tzReport(msg[1])*/
-  if(message.includes('timezone')) {
+  if(message.includes(' timezone')) {
     tzReport();
-  }   
+  } 
 }
 
-//Tell a Name
+//Tell a Weather Forcast of Singapore
 function tzReport(){
-//function tzReport(country){
- // console.log("Within tzReport " + country);
  axios.get('https://api.openweathermap.org/data/2.5/weather?q=Singapore,sg&APPID=ce8c237c706d31d1d46d7bcd755f07b0')
-// axios.get('https://api.openweathermap.org/data/2.5/weather?q=${country}&APPID=ce8c237c706d31d1d46d7bcd755f07b0')
  .then(res => {
     const cc = res.data.name;
     const tz = res.data.timezone;
     const lon = res.data.coord.lon;
     const desc = res.data.main.pressure;
+    const  ws = res.data.wind.speed;
+
 
     const params = {
      icon_emoji: ':sun_with_face:'
    };
  
-   bot.postMessageToChannel('myweatherr', `Timezone for ${cc} is ${tz} and details are ${lon} and ${desc}`, params);
+   bot.postMessageToChannel('myweatherr', `Weather report for ${cc} is \n ${tz} and details are ${lon} and air prressure ${desc}`, params);
 
  });
 }
